9101e2eae86bc23e5f818c5ea19e6a0d7dcc50e6
xaoli123 <3443500966@stumail.ysu.edu.cn>
2024-02-11 23:00:32 +0800

    lab1

--- lab1.py
+++ lab1.py
@@ -5,6 +5,7 @@ def xk(c, d):
         return 6+7+c
     else:
         return 25
+
 def how_big(x):
     if x > 10:
         print('huge')
@@ -14,3 +15,54 @@ def how_big(x):
         print('small')
     else:
         print("nothing")
+
+def falling(n,k):
+   if k==0:
+      print(1)
+   else:
+      a=n
+      for i in range(1,k):
+         n*=a-i
+      print(n)
+
+def divisible_by_k(n,k):
+   if n<k:
+      print(0)
+   else:
+      a=k
+      for i in range(1,n//k+1):
+         k=a*i
+         print(k)
+      print(n//a)
+
+def sum_digits(y):
+   if y<=0:
+      print(false)
+   else:
+      a=str(y)
+      total=sum(int(char) for char in a)
+      print(total)
+
+def ab(c, d):
+     if c > 5:
+         print(c)
+     elif c > 7:
+         print(d)
+     print('foo')
+
+def bake(cake, make):
+     if cake == 0:
+         cake = cake + 1
+         print(cake)
+     if cake == 1:
+         print(make)
+     else:
+         return cake
+     return make
+
+def double_eights(n):
+   a=str(n)
+   for i in range(len(a)):
+      if a[i]=='8' and a[i+1]=='8':
+         return True
+   return False
