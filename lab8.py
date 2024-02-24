6b536a09fcad04e35005a5d3bea965c7a004b1d8
xaoli123 <3443500966@stumail.ysu.edu.cn>
2024-02-24 10:33:11 +0800

    lab8

---
+++ lab8.py
@@ -0,0 +1,21 @@
+#Q2
+class ListNode:
+    def __init__(self,val):
+        self.val=val
+        self.next=None
+def get_link(self):
+    return self.next
+def set_link(self,new_code):
+    self.next=new_node
+def duplicate_link(self, link, val):
+    if self.val==link.val:
+        new_code=Link(link.val,link.next)
+        self.next=new_code
+        return new_code
+
+#Q3
+def convert_link(link):
+    lst = [] 
+    for node in link:
+        lst.append(node.data)
+        return lst
